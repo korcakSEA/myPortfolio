29b2dc63a27bae0bc9df1641b093da3cfa2cccc5
korcakSEA <korhancakir@gmail.com>
2025-10-26 23:20:02 +0200

    Update card height and layout for better readability

--- dist/script/projects.js
+++ dist/script/projects.js
@@ -9,22 +9,22 @@ const projects = [
     techPurpose: "The layout adapts seamlessly across devices using Flexbox and Grid for structure. Hover effects enhance interactivity, providing a dynamic user experience.",
   },
   {
-    name: "Portfolio Website",
-    image: "./assets/images/fycej3bfur1qnf3ixtqq.webp",
-    liveURL: "https://korcaksea.github.io/skilled-elearning-landing-page/",
+    name: "Intro Component with Sign-Up Form",
+    image: "./assets/images/mnmpkjsbvur3xvrydf66.webp",
+    liveURL: "https://korcaksea.github.io/myPortfolio/",
     codeURL: "https://github.com/korcakSEA/myPortfolio.git",
     techs: ["HTML", "SASS", "JavaScript"],
-    purpose: "My personal portfolio toshowcase my work",
-    techPurpose: "Reponsive website with smooth animations and modern design",
+    purpose: "The page presents a clean and modern “intro” section inviting users to join or sign up (for example “Try it free for 7 days” + form)",
+    techPurpose: "Practice building out a sign-up form complete with client-side validation using JavaScript.",
   },
   {
-    name: "Skilled e-learning landing page",
-    image: "./assets/images/fycej3bfur1qnf3ixtqq.webp",
+    name: "Workit landing page",
+    image: "./assets/images/dipoauyeugmgrbbc3g8f.webp",
     liveURL: "https://korcaksea.github.io/skilled-elearning-landing-page/",
     codeURL: "https://github.com/korcakSEA/skilled-elearning-landing-page.git",
     techs: ["HTML", "SASS"],
-    purpose: "The landing page promotes an online learning platform offering various courses",
-    techPurpose: "The layout adapts seamlessly across devices using Flexbox and Grid for structure. Hover effects enhance interactivity, providing a dynamic user experience.",
+    purpose: "The landing page promotes a product data analytics service, emphasizing features like actionable insights, data-driven decisions, and affordable pricing.",
+    techPurpose: "A responsive landing page for a data analytics service.",
   }
 
 ]
